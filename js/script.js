aeb23111f248fd3440c7a2b38b3b77ad7a6ba5b4
AnaGalvis <anaaires@outlook.ie>
2022-08-01 18:57:47 -0400

    Second Commit

--- js/script.js
+++ js/script.js
@@ -1,29 +1,52 @@
-
 const nombreIngresado = prompt("Ingresa tu nombre") 
 const apellidoIngresado= prompt ("Ingresar Apellido")
 const direccion = prompt ("Ingresar direccion")
-const price = 1200
 
-let quantity = parseInt(prompt ("cuantos productos desea hoy?"))
+    
+class producto {
+ constructor(id, nombreProducto, precio,){
+    this.id = id;
+    this.nombreProducto= nombreProducto;
+    this.precio = precio;
+ }
 
-    if ( quantity> 0 ){
-        let total = price * quantity;
-        alert ("Su total es: "  +total +" mas un delivery de 460 y un descuento de 100 pesos por el dia de hoy");
-        const suma = (a,b) => a + b
-        const resta = (a,b) => a-b 
-        const descuento = 100;
-        const delivery = 460;
 
-        let precioTotal = resta (suma (total, delivery), descuento)
+mostrar_descripcion_completa(){
+return this.id + this.nombreProducto + " $" + this.precio ;
+}
+}
 
-        let respuesta = confirm("Confirma que todos los datos ingresados son correctos? "  + "  Nombre: " + nombreIngresado  + "  Direccion: " +direccion  + " Su total seria: " +precioTotal)
-     alert ("Su delivery tardara entre 30-45 minutos. Gracias por su compra")
 
-    }else{
-        alert ("No ingresaste un dato valido")
+
+ let mis_productos= new Array();
+ mis_productos.push(new producto("Platillo1","Avocado Brusqueta con tomates confitados", 700,));
+ mis_productos.push(new producto("Platillo2","Las triplets toasts", 1000,));
+ mis_productos.push(new producto("Platillo3","Pizza de ahuyama", 1100));
+ mis_productos.push(new producto("Platillo4","The queen Burguer", 1200));
+ mis_productos.push(new producto("Platillo5","Tacos to share", 1600));
+ mis_productos.push(new producto("Platillo6","Brunch for the team", 1850));
+
+ let respuesta = prompt("Estas listo para ver el menu? \n 1) Ver Menu \n 2) No, quizas mas tarde ");
+
+    if (respuesta == "1"){
+        alert (" Vamos Vegan Menu: \n" +mostrar_menu() + "  les gustaria ingresar algun producto? ")
+
+   
+      }else if(respuesta == "2"){
+       alert ("Bienvenido, Sigue mirando!")
+   
+      }else{
+      alert ("No ingreso un dato valido")
+      }
+
+ function mostrar_menu(){
+    let menu = "";
+    for (let i= 0; i< mis_productos.length; i++){
+        menu += mis_productos[i].mostrar_descripcion_completa();
+
+    
     }
+return menu;
+ }
 
-
-
-
- 
+ alert ("Para pedir por favor llama al 561 510 6372, Gracias")
